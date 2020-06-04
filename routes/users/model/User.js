bb8c8facc03c7e635b95d68df912e23fbebfb625
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2020-06-04 14:58:53 -0400

    added a mongoose model to the users/model/User.js file

---
+++ routes/users/model/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const moment = require('moment')
+const now = moment()
+
+const UserSchema = mongoose.Schema({
+    username:{
+        type:String,
+        trim:true,
+        required:'username is unique',
+        unique:'Username already exists,please choose another'
+        
+    },
+    email:{
+        type:String,
+        trim:true,
+        required:'Email is required',
+        unique:'Email already has a user',
+        match:[/.+@..+/,'Please enter a valid email']
+        
+    },
+    password:{
+        type:String,
+        required:'password us required',
+
+    },
+
+    city:{
+        type:String,
+        trim:true,
+        match:[/^[_A-z0-9]*((-|\s)*[_A-z0-9])*$/g,"no special characters"]
+    },
+    state:{
+        type:String,
+        trim:true,
+        match:[/^[_A-z0-9]*((-|\s)*[_A-z0-9])*$/g,"no special characters"]
+    },
+    userCreated:{
+        type:String,
+        default:now.format('dddd,MMMM Do YYY,h:mm:ss a')
+    },
+    
+    wins:{
+        type:Array,
+    },
+
+    losses:{
+        type:Array,
+    },
+
+
+})
